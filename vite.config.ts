aed1cbc0c408b8f28ef6474f338fcf14741c9e94
Chester Cari <chester_cari@outlook.com>
2023-04-02 03:46:49 -0400

    added dependency and setup glsl vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    glsl({
+      exclude: 'node_modules/**',
+      // change this to match the location of your shader files
+      include: ['src/**/**/*.glsl', '/\.glsl$/' ],
+    })],
 })
