6d064b9b9e8f880843466ec44c071e9cb8117e20
Chester Cari <chester_cari@outlook.com>
2023-04-03 19:18:28 -0400

    failed experiment to dynamically load components from filestructure

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import "./App.css";
 import Experience from "./components/Experience";
 import { Canvas } from "@react-three/fiber";
 import Overlay, { OverlayDark, OverlayLight } from "./components/Overlay";
+import { useControls } from "leva";
 
 function App() {
+  const controls = useControls({
+    backgroundColor: "#1f1f1f",
+  });
+
   return (
     <>
       <Overlay />
-      <Canvas camera={{ position: [3, 3, 3] }}>
+      <Canvas
+        style={{ background: controls.backgroundColor }}
+        camera={{ position: [3, 3, 3] }}
+      >
         <Experience />
       </Canvas>
     </>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,10 +1,7 @@
-import { Grid, OrbitControls, shaderMaterial } from "@react-three/drei";
-import { MaterialNode, extend, useFrame } from "@react-three/fiber";
+import { Grid, OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
-import React, { useRef, useState } from "react";
-import Portal from "../shaders/portal/Portal";
-import Waves from "../shaders/waves/Waves";
+import React, { Suspense, useState } from "react";
 
 type Props = {};
 
@@ -21,15 +18,21 @@ const Experience = (props: Props) => {
       },
     },
   });
+
+  const DynamicComponent = React.lazy(() =>
+    component === "Portal"
+      ? import("../shaders/portal/Portal")
+      : import("../shaders/waves/Waves")
+  );
+
   return (
     <>
       <Perf position={"top-left"} />
       <OrbitControls />
       <Grid cellColor="white" args={[10, 10]} />
-      <>
-        {component === "Portal" ? <Portal /> : null}
-        {component === "Waves" ? <Waves /> : null}
-      </>
+      <Suspense fallback={null}>
+        <DynamicComponent />
+      </Suspense>
     </>
   );
 };
